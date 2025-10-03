cf5a2f7e9522c0e43d8aa0615a65c218c6471f31
William Walker <turner.walker79@gmail.com>
2025-10-03 15:34:21 -0400

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;
