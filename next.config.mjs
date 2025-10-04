67707a27806631bb72b552f2c7db47bcb5b76962
William Walker <turner.walker79@gmail.com>
2025-10-04 02:54:28 -0400

    delete

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  experimental: {
-    forceSwcTransforms: true,
-  },
-};
-
-export default nextConfig;
