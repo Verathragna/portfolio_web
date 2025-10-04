a5617694acbc4d704bbc30614d189620922f3bf6
William Walker <turner.walker79@gmail.com>
2025-10-04 02:52:18 -0400

    Updated package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  experimental: {
-    forceSwcTransforms: true,
+  swcMinify: false,
+  compiler: {
+    removeConsole: false,
   },
 };
 
